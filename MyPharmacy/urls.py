8505541b3ab99fda119213d0418acc84fd4a5fcb
delta@Umuhire <anuarithemuhire@gmail.com>
2023-03-21 00:59:26 -0700

    crud operation

--- MyPharmacy/urls.py
+++ MyPharmacy/urls.py
@@ -5,8 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('pharmacist/',Pharmacist_view.as_view(), name='pharm'),
-    
+    path('pharmacist/<int:pk>/',Pharmacist_view.as_view(), name='pharm-details'),
     path('med/',MedicalType_view.as_view(), name='med'),
-    path('med/<int:pk>',MedicalType_details.as_view(), name='med_details'),
+    path('med/<int:pk>/',MedicalType_view.as_view(), name='med_details'),
 
 ]
--- MyPharmacy/views.py
+++ MyPharmacy/views.py
@@ -40,6 +40,12 @@ class Pharmacist_view(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, id=None):
+        pharmacist = Pharmacist.objects.filter(id=id)
+        pharmacist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
   
   
    
@@ -61,12 +67,12 @@ class MedicalType_view(APIView):
         '''
         Create the medecine Types with given data
         '''
-        data = {
-            'Type_name': request.data.get('Type_name'), 
-            'Created_by': request.user.id,
-            'Created_date': request.data.get('Created_date'), 
-        }
-        serializer = Medical_Type_Selializers(data=data)
+        # data = {
+        #     'Type_name': request.data.get('Type_name'), 
+        #     'Created_by': request.user.id,
+        #     'Created_date': request.data.get('Created_date'), 
+        # }
+        serializer = Medical_Type_Selializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -74,23 +80,9 @@ class MedicalType_view(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     
-    
-class MedicalType_details(APIView):
-    def get_object(self, pk):
-        try:
-            medical_type = Medicaltype.objects.get(id=pk)
-            
-        except medical_type.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-    def get(self, request, pk=None):
-        medecine_type = self.get_object(pk)
-        serializer = Medical_Type_Selializers(medecine_type)
-        return Response(serializer.data)
-    
     def put(self, request, pk=None):
-        medecine_type = self.get_object(pk)
+        # medecine_type = self.get_object(pk)
+        medecine_type = Medicaltype.objects.get(id=pk)
         serializer = Medical_Type_Selializers(medecine_type, data=request.data)
         
         if serializer.is_valid():
@@ -98,22 +90,32 @@ class MedicalType_details(APIView):
             return Response(serializer.data)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
-    
-    
-    
-    
-class MedicaltypeViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving medecine types.
-    """
-    def list(self, request):
-        medical_type = User.objects.all()
-        serializer = Medical_Type_Selializers(medical_type, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        medical_type = User.objects.all()
-        medtype= get_object_or_404(medical_type, pk=pk)
-        serializer = Medical_Type_Selializers(medtype)
-        return Response(serializer.data)
+    
+    
+    
+    def delete(self, request, pk=None):
+        # medecine_type = self.get_object(pk)
+        medecine_type = Medicaltype.objects.get(id=pk)
+        medecine_type.delete()
+        return Response({"status":"deleted"})
+    
+    
+    
+    
+    
+# class MedicaltypeViewSet(viewsets.ViewSet):
+#     """
+#     A simple ViewSet for listing or retrieving medecine types.
+#     """
+#     def list(self, request):
+#         medical_type = User.objects.all()
+#         serializer = Medical_Type_Selializers(medical_type, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         medical_type = User.objects.all()
+#         medtype= get_object_or_404(medical_type, pk=pk)
+#         serializer = Medical_Type_Selializers(medtype)
+#         return Response(serializer.data)
     
